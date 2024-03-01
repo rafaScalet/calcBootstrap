f47a7b2c2b8a6fbaadadfa34737fd1ebe84b32d3
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-01 17:00:29 -0300

    added the 'ope' function, for operator buttons

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -9,10 +9,17 @@ export class CalculatorComponent {
   num1: number = 0
   num2: number = 0
   result: any = 0
+  operation: string = ""
   text: string = ""
 
   set(text: string = ""){
-    this.text = this.text + text;
-    this.num1 = parseFloat(this.text);
+    this.text = this.text + text
+    this.num1 = parseFloat(this.text)
+  }
+
+  ope(operation: string = ""){
+    this.operation = operation
+    this.num2 = this.num1
+    this.text = ""
   }
 }
