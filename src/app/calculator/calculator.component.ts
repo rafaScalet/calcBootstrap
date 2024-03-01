ecba43ddf06339422add8a65ef3d6b8813df6adf
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-01 16:43:10 -0300

    added the set function, for number buttons

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  num1: number = 0
+  num2: number = 0
+  result: any = 0
+  text: string = ""
 
+  set(text: string = ""){
+    this.text = this.text + text;
+    this.num1 = parseFloat(this.text);
+  }
 }
