4e730363bdc32135a6e77d0394ad9051ecd0e334
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-01 23:11:59 -0300

    'calc' and 'bin' functions merged into one, and changing the name of the 'ce' function to 'clear'

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,40 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
 })
 export class CalculatorComponent {
-  num1: number = 0
-  num2: number = 0
-  result: any = 0
-  memory: string[] = []
-  operation: string = ""
-  text: string = ""
+  num1: number = 0;
+  num2: number = 0;
+  result: any = 0;
+  memory: string[] = [];
+  operation: string = '';
+  text: string = '';
 
-  set(text: string = ""){
-    this.text = this.text + text
-    this.num1 = parseFloat(this.text)
+  set(text: string = '') {
+    this.text = this.text + text;
+    this.num1 = parseFloat(this.text);
   }
 
-  ope(operation: string = ""){
-    this.operation = operation
-    this.num2 = this.num1
-    this.text = ""
+  ope(operation: string = '') {
+    this.operation = operation;
+    this.num2 = this.num1;
+    this.text = '';
   }
 
-  calc(){
-    this.result = eval(this.num1 + this.operation + this.num2)
+  calc() {
+    this.result = eval(this.num1 + this.operation + this.num2);
+    this.memory.push(this.num1 + this.operation + this.num2 + '= ' + this.result);
+    this.text = '';
   }
 
-  ce(){
-    this.num1 = 0
-    this.num2 = 0
-    this.result = 0
-    this.operation = ""
-    this.text = ""
-  }
-
-  bin(){
-    this.memory.push(this.num1 + this.operation + this.num2 + "= " + this.result)
+  clear() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.result = 0;
+    this.operation = '';
+    this.text = '';
   }
 }
